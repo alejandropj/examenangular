040cb96f43aa28cccc282cefb1e76994279c71a7
alejandropj <alejandroparrajimenez@gmail.com>
2023-11-20 13:53:11 +0100

    Version Final

--- src/app/components/comprar/comprar.component.ts
+++ src/app/components/comprar/comprar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cubo } from 'src/app/models/Cubo';
+import { ServiceCubos } from 'src/app/services/service.cubos';
 
 @Component({
   selector: 'app-comprar',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./comprar.component.css']
 })
 export class ComprarComponent {
+  public checkToken!: boolean;
+  public cubos !: Array<Cubo>;
+  public cuboSeleccionado:any;
+
+  constructor(private _service: ServiceCubos, private _router: Router){}
+
+
+  ngOnInit(): void {
+    this.checkToken = this.localStorageItem();
+    if(!this.checkToken){
+      this._service.getCubos().subscribe(response=>{
+        this.cubos = response
+      })
+    }
+
+  }
+  public localStorageItem(): boolean {
+    if (localStorage.getItem("token") === null) {
+      return true
+    } else {
+      return false;
+    };
+  }
+
+  public enviarDatos():void {
+    console.log(this.cuboSeleccionado);
+    this._service.postCompra(this.cuboSeleccionado).subscribe(res=>{
+      this._router.navigate(["/compras"]);
+    })
+  }
 
 }
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/User';
+import { ServiceCubos } from 'src/app/services/service.cubos';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
+  public checkToken!: boolean;
+  public usuario !: User;
+  public mensaje !: string;
+
+  constructor(private _service: ServiceCubos, private _router: Router) {
+    this.usuario = new User(1, "", "", "");
+
+  }
+  ngOnInit(): void {
+    this.checkToken = this.localStorageItem();
+  }
+  public localStorageItem(): boolean {
+    if (localStorage.getItem("token") === null) {
+      return true
+    } else {
+      return false;
+    };
+  }
+
+  enviarDatos() {
+    console.log(this.usuario)
+    this._service.registro(this.usuario).subscribe(response => {
+      this.mensaje = "";
+      //console.log(response)
+      localStorage.setItem("token", response.response);
+      this._router.navigate(["/perfil"]);
+      console.log(response.response)
+    }, err => {
+      this.mensaje = "Error interno."
+    })
+  }
 
 }
